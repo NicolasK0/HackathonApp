bf6cb1586c96acd7046cb4b13c5c01c167ad1e40
NicolasK0 <nkous05@gmail.com>
2025-04-06 10:36:01 -0400

    Initial Commit

---
+++ HackathonApp/HackathonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackathonAppApp.swift
+//  HackathonApp
+//
+//  Created by Nicolas Kousoulas on 4/6/25.
+//
+
+import SwiftUI
+
+@main
+struct HackathonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HackathonAppTests/HackathonAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  HackathonAppTests.swift
+//  HackathonAppTests
+//
+//  Created by Nicolas Kousoulas on 4/6/25.
+//
+
+import Testing
+@testable import HackathonApp
+
+struct HackathonAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
